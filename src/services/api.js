dbc7c28fe1c0377c7da70f0410a483580773d7a0
Osama-tec <hashmiosama555@gmail.com>
2025-02-10 01:09:22 +0500

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3100',
+  baseURL: 'https://dropnshop-backend.onrender.com',
 });
 
 const mlApi = axios.create({
